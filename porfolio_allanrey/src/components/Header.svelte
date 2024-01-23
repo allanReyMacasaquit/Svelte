41675724117bbf5b4de1336c005607f0588d07b4
allan <allanrey.macasaquit@gmail.com>
2024-01-22 22:44:21 -0500

    Create header main and footer components.

---
+++ porfolio_allanrey/src/components/Header.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let y = 0
+
+    export let tabs = [
+        { name: "Projects ", link: "#projects" },
+        { name: "About me", link: "#about" },
+      
+    ];
+</script>
+
+<header
+    class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
+        (y > 0
+            ? " py-4 bg-slate-950 border-violet-950"
+            : " py-6 bg-transparent border-transparent")}
+>
+    <h1 class="font-medium uppercase">
+        <b class="font-bold montserrat">Allan rey</b> <span class="text-blue-600 font-bold">Macasaquit</span>
+    </h1>
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a
+                href={tab.link}
+                class="duration-200 hover:text-slate-400"
+                target={index === 2 ? "_blank" : ""}
+            >
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+        
+        <button
+            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white hover:text-white text-slate-950"
+        >
+            <div
+                class="absolute top-0 right-full w-full h-full bg-blue-600 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9 ">Get in touch</h4>
+        </button>
+    </div>
+</header>
--- porfolio_allanrey/src/routes/+layout.svelte
+++ porfolio_allanrey/src/routes/+layout.svelte
@@ -1,5 +1,27 @@
 <script>
-  import "../app.css";
-</script>
+  import "../app.css"
+	import Footer from "../components/Footer.svelte"
+	import Header from "../components/Header.svelte"
 
-<slot />
+  let y
+  let innerHeight = 0
+  let innerWidth = 0
+
+  function topView() {
+    document.body.scrollIntoView()
+  }
+
+</script>
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+  <div class={"fix bottom-0 w-full duration-200 flex p-5 z-[10]" + ( y > 0 ? 'opacity-100 pointer-events-auto': 'pointer-events-none opacity-0')}>
+    <button on:click={topView} class="ml-auto rounded-full bg-slate-600 text-violet-400 px-3 sm:px-4 hover:bg-slate-600 cursor-pointer aspect-square grid place-items-center">
+      <i class="fa-solid fa-arrow-up"></i>
+    </button>
+  </div>
+
+  <Header y = {y}/>
+  <slot />
+  <Footer/>
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth/>
